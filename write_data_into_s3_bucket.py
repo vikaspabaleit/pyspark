d93f2af5b00ec959b35fc5456b4cdb80b620ce24
Vikas S Pabale <vikaspabaleit@gmail.com>
2025-07-29 23:13:16 +0530

    Update write_data_into_s3_bucket.py

--- write_data_into_s3_bucket.py
+++ write_data_into_s3_bucket.py
@@ -2,4 +2,16 @@
  Write PySpark code to save a DataFrame in Parquet format to an S3 bucket. 
  Explain how to overwrite a file stored in S3 using PySpark. * /
 
-from 
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession.builder.appName("write_data_into_s3_bucket")getOrCreate()
+
+data = [ (1,"Durva"),(2,"Aaru") ]
+schemas = [ "id","Name"]
+df = spark.createDataFrame(data,schemas)
+df.printSchema()
+df.show()
+df.write.mode("overwrite").parquet("s3://bucket-name/folder")
+
+
